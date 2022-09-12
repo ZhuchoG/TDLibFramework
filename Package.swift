838046feede9b09480d02179a197e0463590e385
Vitaliy Zhukov <vi.zhukov@tinkoff.ru>
2022-09-12 17:02:34 +0700

    fix

--- Package.swift
+++ Package.swift
@@ -29,8 +29,7 @@ let package = Package(
         ),
         .binaryTarget(
             name: "TDLibFramework",
-            url: "https://github.com/Swiftgram/TDLibFramework/releases/download/1.8.4-07b7faf6/TDLibFramework.zip",
-            checksum: "80dea8e1a60091d79b946a0f0360f81650b80476cd505a00c4b550cd6de0069f"
+            url: "https://github.com/Swiftgram/TDLibFramework/releases/download/1.8.5/TDLibFramework.xcframework"
         )
     ]
 )
